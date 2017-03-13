61488462e16c69246b940c101f1ca43e2e53ac72
thom <tom@tksfz.org>
2017-03-13 14:12:53 -0700

    Add Add command

---
+++ src/main/scala/org/tksfz/doto/AddCommandExec.scala
@@ -0,0 +1,16 @@
+package org.tksfz.doto
+
+import java.nio.file.Paths
+import java.util.UUID
+
+import org.tksfz.doto.repo.Repo
+
+object AddCommandExec extends CommandExec[Add] {
+  override def execute(c: Config, t: Add): Unit = {
+    val repo = new Repo(Paths.get(""))
+    val parentUuid = UUID.randomUUID() // UUID.fromString(t.parentId)
+    val uuid = UUID.randomUUID()
+    val doc = Task(uuid, false, Some(IdRef(parentUuid)), t.subject)
+    repo.put(uuid, doc)
+  }
+}
--- src/main/scala/org/tksfz/doto/Main.scala
+++ src/main/scala/org/tksfz/doto/Main.scala
@@ -4,23 +4,25 @@ import java.io.File
 
 import scopt.{OptionDef, Read}
 
+case class Config(cmd: Option[Command] = None)
+
+sealed trait Command
+case class Init(location: Option[File]) extends Command
+case class Add(parentId: String, subject: String) extends Command
+case object List extends Command
+
 trait CommandExec[T] {
-  def execute(t: T): Unit
+  def execute(c: Config, t: T): Unit
 }
 
 object Main {
 
-  case class Config(cmd: Option[Command] = None)
-
-  sealed trait Command
-  case class Init(location: Option[File]) extends Command
-  case class Add(subject: String) extends Command
-  case object List extends Command
-
   def main(args: Array[String]): Unit = {
     parser.parse(args, Config()) match {
-      case Some(Config(Some(Init(loc)))) =>
-        println(loc)
+      case Some(c@Config(Some(cmd))) => cmd match {
+        case add: Add => AddCommandExec.execute(c, add)
+        case _ => println(c)
+      }
       case Some(config) =>
         println(config)
       case None =>
@@ -34,9 +36,14 @@ object Main {
     cmd("init").action((_, c) => c.copy(cmd = Some(Init(None))))
       .text("Initialize a new doto repo.")
       .children(
-        arg[File]("location").optional().cmdaction[Init]((x, c) =>
-          c.copy(location = Some(x))
-        )
+        arg[File]("location").optional().cmdaction[Init]((x, c) => c.copy(location = Some(x)))
+      )
+
+    cmd("add").action((_, c) => c.copy(cmd = Some(Add("", ""))))
+      .text("Add a task, event, or thread")
+      .children(
+        opt[String]('p', "parent").valueName("parent").cmdaction[Add]((x, c) => c.copy(parentId = x)),
+        opt[String]('s', "subject").valueName("subject").cmdaction[Add]((x, c) => c.copy(subject = x))
       )
   }
 
