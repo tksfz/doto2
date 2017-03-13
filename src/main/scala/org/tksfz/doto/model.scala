544d76e05185aee29c2c09d490ca9f5fb9504640
thom <tom@tksfz.org>
2017-03-13 13:48:14 -0700

    Add type Id = UUID

--- src/main/scala/org/tksfz/doto/model.scala
+++ src/main/scala/org/tksfz/doto/model.scala
@@ -1,40 +1,40 @@
 package org.tksfz.doto
 
-trait HasId { def id: Int }
+trait HasId { def id: Id }
 
 sealed abstract class Ref[T <: HasId]
-case class IdRef[T <: HasId](id: Int) extends Ref[T]
+case class IdRef[T <: HasId](id: Id) extends Ref[T]
 case class ValueRef[T <: HasId](t: T) extends Ref[T]
 
 trait HasChildren[T <: HasId] { def children: List[Ref[T]] }
 
 trait Completable { def completed: Boolean }
 
-abstract class Node[T <: HasId] extends HasId with Completable with HasChildren[T] {
+sealed abstract class Node[T <: HasId] extends HasId with Completable with HasChildren[T] {
   def subject: String
 }
 
-abstract class Work extends Node[Task] {
+sealed abstract class Work extends Node[Task] {
   def target: Option[Ref[Event]]
 }
 
 case class Task(
-  override val id: Int,
+  override val id: Id,
   override val completed: Boolean,
-  override val children: List[Ref[Task]],
   override val target: Option[Ref[Event]],
-  override val subject: String
+  override val subject: String,
+  override val children: List[Ref[Task]] = Nil
 ) extends Work
 
 /**
   * Effectively, changes are buffered.
   */
 case class Event(
-  override val id: Int,
+  override val id: Id,
   override val completed: Boolean,
-  override val children: List[Ref[Task]],
   override val target: Option[Ref[Event]],
-  override val subject: String
+  override val subject: String,
+  override val children: List[Ref[Task]] = Nil
 ) extends Work
 
 /**
@@ -43,10 +43,10 @@ case class Event(
   * Where do we put automatic completion conditions?
   */
 case class Thread[T <: Work](
-  override val id: Int,
+  override val id: Id,
   override val completed: Boolean,
   parent: Option[Ref[Thread[T]]],
-  override val children: List[Ref[T]],
-  override val subject: String
+  override val subject: String,
+  override val children: List[Ref[T]] = Nil
 ) extends Node[T]
 
---
+++ src/main/scala/org/tksfz/doto/package.scala
@@ -0,0 +1,7 @@
+package org.tksfz
+
+import java.util.UUID
+
+package object doto {
+  type Id = UUID
+}
