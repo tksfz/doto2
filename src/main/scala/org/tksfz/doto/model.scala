325a50d7c3b0e6eb4e852b497b042d6b8341af32
thom <tom@tksfz.org>
2017-03-13 14:10:44 -0700

    Add circe codecs for models

--- src/main/scala/org/tksfz/doto/model.scala
+++ src/main/scala/org/tksfz/doto/model.scala
@@ -2,7 +2,12 @@ package org.tksfz.doto
 
 trait HasId { def id: Id }
 
-sealed abstract class Ref[T <: HasId]
+sealed abstract class Ref[T <: HasId] {
+  def toIdRef: IdRef[T] = this match {
+    case x@IdRef(_) => x
+    case ValueRef(t) => IdRef(t.id)
+  }
+}
 case class IdRef[T <: HasId](id: Id) extends Ref[T]
 case class ValueRef[T <: HasId](t: T) extends Ref[T]
 
@@ -50,3 +55,25 @@ case class Thread[T <: Work](
   override val children: List[Ref[T]] = Nil
 ) extends Node[T]
 
+import io.circe.Decoder.Result
+import io.circe._
+import io.circe.generic.semiauto._
+
+object Ref {
+  implicit def idRefEncoder[T <: HasId]: Encoder[IdRef[T]] = deriveEncoder[IdRef[T]]
+  implicit def idRefDecoder[T <: HasId]: Decoder[IdRef[T]] = deriveDecoder[IdRef[T]]
+
+  implicit def refEncoder[T <: HasId]: Encoder[Ref[T]] = new Encoder[Ref[T]] {
+    override def apply(a: Ref[T]): Json = Encoder[IdRef[T]].apply(a.toIdRef)
+  }
+
+  // do we need this?
+  implicit def refDecoder[T <: HasId]: Decoder[Ref[T]] = new Decoder[Ref[T]] {
+    override def apply(c: HCursor): Result[Ref[T]] = Decoder[IdRef[T]].apply(c)
+  }
+}
+
+object Task {
+  implicit val taskEncoder = deriveEncoder[Task]
+  implicit val taskDecoder = deriveDecoder[Task]
+}
