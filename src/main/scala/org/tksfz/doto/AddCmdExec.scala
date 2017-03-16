d37307f8dff3b2a9c61e7a1b85288d65f5881c5b
thom <tom@tksfz.org>
2017-03-15 17:39:54 -0700

    Tasks parented to tasks

--- src/main/scala/org/tksfz/doto/AddCmdExec.scala
+++ src/main/scala/org/tksfz/doto/AddCmdExec.scala
@@ -7,22 +7,26 @@ import org.tksfz.doto.repo.Repo
 
 object AddCmdExec extends CmdExec[Add] {
   override def execute(c: Config, add: Add): Unit = {
+    val uuid = UUID.randomUUID()
     val repo = new Repo(Paths.get(""))
-    val parentOpt = repo.threads.findByIdPrefix(add.parentId)
-    parentOpt map { parent =>
-      val uuid = UUID.randomUUID()
-      parent.`type`.apply match {
+    repo.threads.findByIdPrefix(add.parentId) map { parentThread =>
+      parentThread.`type`.apply match {
         case TaskWorkType =>
           val doc = Task(uuid, add.subject)
-          val newParent = parent.asInstanceOf[Thread[Task]].copy[Task](children = parent.children.asInstanceOf[List[Ref[Task]]] :+ ValueRef(doc))
+          val newParent = parentThread.asInstanceOf[Thread[Task]].copy[Task](children = parentThread.children.asInstanceOf[List[Ref[Task]]] :+ ValueRef(doc))
           repo.tasks.put(uuid, doc)
           repo.threads.put(newParent.id, newParent)
         case EventWorkType =>
           val doc = Event(uuid, add.subject)
-          val newParent = parent.asInstanceOf[Thread[Event]].copy[Event](children = parent.children.asInstanceOf[List[Ref[Event]]] :+ ValueRef(doc))
+          val newParent = parentThread.asInstanceOf[Thread[Event]].copy[Event](children = parentThread.children.asInstanceOf[List[Ref[Event]]] :+ ValueRef(doc))
           repo.events.put(uuid, doc)
           repo.threads.put(newParent.id, newParent)
       }
+    } orElse repo.tasks.findByIdPrefix(add.parentId).map { parentTask =>
+      val doc = Task(uuid, add.subject)
+      val newParent = parentTask.copy(children = parentTask.children :+ ValueRef(doc))
+      repo.tasks.put(uuid, doc)
+      repo.tasks.put(newParent.id, newParent)
     } getOrElse {
       println("Could not find parent with id starting with " + add.parentId)
     }
