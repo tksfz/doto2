61126949e0a523fe7aac97966a7b91983825be61
thom <tom@tksfz.org>
2017-03-17 12:58:43 -0700

    Allow complete cmd to work with events

--- src/main/scala/org/tksfz/doto/main/CompleteCmdExec.scala
+++ src/main/scala/org/tksfz/doto/main/CompleteCmdExec.scala
@@ -10,11 +10,16 @@ import org.tksfz.doto.repo.Repo
 object CompleteCmdExec extends CmdExec[Complete] {
   override def execute(c: Config, cmd: Complete): Unit = {
     val repo = new Repo(Paths.get(""))
+    // TODO: how do we abstract this code so that it can operate over
+    // both tasks and events?
     repo.tasks.findByIdPrefix(cmd.id) map { task =>
       val newTask = task.copy(completed = true)
       repo.tasks.put(task.id, newTask)
+    } orElse repo.events.findByIdPrefix(cmd.id).map { event =>
+      val newEvent = event.copy(completed = true)
+      repo.events.put(event.id, newEvent)
     } getOrElse {
-      println("Couldn't find task with id starting with '" + cmd.id + "'")
+      println("Couldn't find task or event with id starting with '" + cmd.id + "'")
     }
   }
 }
