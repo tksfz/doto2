75deac61c09639d716842ad283f6e1f69dc59fca
thom <tom@tksfz.org>
2017-03-18 16:05:05 -0700

    "Move" command (set -p)

--- src/main/scala/org/tksfz/doto/main/SetCmdExec.scala
+++ src/main/scala/org/tksfz/doto/main/SetCmdExec.scala
@@ -2,6 +2,7 @@ package org.tksfz.doto.main
 
 import java.nio.file.Paths
 
+import org.tksfz.doto.{IdRef, Task}
 import org.tksfz.doto.repo.Repo
 
 /**
@@ -11,10 +12,27 @@ object SetCmdExec extends CmdExec[Set] {
   override def execute(c: Config, cmd: Set): Unit = {
     val repo = new Repo(Paths.get(""))
     repo.tasks.findByIdPrefix(cmd.id) map { task =>
-      val newTask = task.copy(subject = cmd.newSubject)
-      repo.tasks.put(task.id, newTask)
+      cmd.newSubject foreach { newSubject =>
+        val newTask = task.copy(subject = newSubject)
+        repo.put(newTask)
+      }
+
+      cmd.newParent foreach { move(repo, task, _) }
     } getOrElse {
       println("Couldn't find task with id starting with '" + cmd.id + "'")
+    }
+  }
+
+  private[this] def move(repo: Repo, task: Task, newParentId: String) = {
+    // new and old parent could both be either threads or tasks
+    repo.findTaskOrTaskThreadByIdPrefix(newParentId) map { newParent =>
+      // TODO: local indexing
+      repo.findTaskParent(task.id) map { oldParent =>
+        val oldParentModified = oldParent.modifyChildren(_.filterNot(_.id == task.id))
+        val newParentModified = newParent.modifyChildren(_ :+ IdRef[Task](task.id))
+        repo.dynamicPut(newParentModified)
+        repo.dynamicPut(oldParentModified)
+      }
     }
   }
 }
