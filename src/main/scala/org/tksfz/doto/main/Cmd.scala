117ad21db1062b843147767c124bf20c6d998b35
thom <tom@tksfz.org>
2017-03-23 17:17:17 -0700

    Move Cmd and CmdExec to a separate file

---
+++ src/main/scala/org/tksfz/doto/main/Cmd.scala
@@ -0,0 +1,37 @@
+package org.tksfz.doto.main
+
+import java.io.File
+
+/**
+  * Created by thom on 3/23/17.
+  */
+sealed trait Cmd
+case class Init(location: Option[File]) extends Cmd
+case class Add(parentId: String, subject: String) extends Cmd
+case class ThreadCmd(parentId: String, subject: String, isEvent: Boolean = false) extends Cmd
+case class ListCmd(ignoreFocus: Boolean = false) extends Cmd
+case class Complete(id: String) extends Cmd
+case class Set(id: String, newSubject: Option[String] = None, newParent: Option[String] = None) extends Cmd
+case class Plan(taskId: String, eventId: String) extends Cmd
+case class Focus(id: String) extends Cmd
+
+trait CmdExec[T] {
+  def execute(c: Config, cmd: T): Unit
+}
+
+object CmdExec {
+  def execute(c: Config) = c match {
+    case Config(Some(cmd)) => cmd match {
+      case add: Add => AddCmdExec.execute(c, add)
+      case thread: ThreadCmd => ThreadCmdExec.execute(c, thread)
+      case ls: ListCmd => ListCmdExec.execute(c, ls)
+      case init: Init => InitCmdExec.execute(c, init)
+      case complete: Complete => CompleteCmdExec.execute(c, complete)
+      case set: Set => SetCmdExec.execute(c, set)
+      case plan: Plan => PlanCmdExec.execute(c, plan)
+      case focus: Focus => FocusCmdExec.execute(c, focus)
+      case _ => println(c)
+    }
+    case Config(None) => ()
+  }
+}
--- src/main/scala/org/tksfz/doto/main/Main.scala
+++ src/main/scala/org/tksfz/doto/main/Main.scala
@@ -6,35 +6,12 @@ import scopt.{OptionDef, Read}
 
 case class Config(cmd: Option[Cmd] = None)
 
-sealed trait Cmd
-case class Init(location: Option[File]) extends Cmd
-case class Add(parentId: String, subject: String) extends Cmd
-case class ThreadCmd(parentId: String, subject: String, isEvent: Boolean = false) extends Cmd
-case class ListCmd(ignoreFocus: Boolean = false) extends Cmd
-case class Complete(id: String) extends Cmd
-case class Set(id: String, newSubject: Option[String] = None, newParent: Option[String] = None) extends Cmd
-case class Plan(taskId: String, eventId: String) extends Cmd
-case class Focus(id: String) extends Cmd
-
-trait CmdExec[T] {
-  def execute(c: Config, cmd: T): Unit
-}
-
 object Main {
 
   def main(args: Array[String]): Unit = {
     parser.parse(args, Config()) match {
-      case Some(c@Config(Some(cmd))) => cmd match {
-        case add: Add => AddCmdExec.execute(c, add)
-        case thread: ThreadCmd => ThreadCmdExec.execute(c, thread)
-        case ls: ListCmd => ListCmdExec.execute(c, ls)
-        case init: Init => InitCmdExec.execute(c, init)
-        case complete: Complete => CompleteCmdExec.execute(c, complete)
-        case set: Set => SetCmdExec.execute(c, set)
-        case plan: Plan => PlanCmdExec.execute(c, plan)
-        case focus: Focus => FocusCmdExec.execute(c, focus)
-        case _ => println(c)
-      }
+      case Some(c@Config(Some(_))) =>
+        CmdExec.execute(c)
       case Some(config) =>
         println(config)
       case None =>
