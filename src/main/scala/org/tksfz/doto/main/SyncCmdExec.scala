cb3a02a5453a2d07ad2a6ba2960dcee50b9cad8d
thom <tom@tksfz.org>
2017-04-05 17:21:32 -0700

    Sync command has better messages

--- src/main/scala/org/tksfz/doto/main/SyncCmdExec.scala
+++ src/main/scala/org/tksfz/doto/main/SyncCmdExec.scala
@@ -15,9 +15,10 @@ object SyncCmdExec extends CmdExec[Sync] {
     if (!project.hasRemote) {
       println(s"No remote for ${project.name}")
     } else {
+      println(s"Syncing with '${project.remote.get}'...")
       project.sync(cmd.noPull) match {
         case Success(result) =>
-          println(s"Synced with '${result.remote}'")
+          println(s"Synced.")
           // TODO: report the number of synced objects
         case Failure(e: TransportException) if e.getMessage contains "Nothing to fetch" =>
           println("Sync failed. If this is a new repo, use 'sync -n'.")
