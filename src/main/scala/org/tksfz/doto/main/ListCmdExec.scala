dc28c5fdb2608a31abbf3ee03e3baf2d39544ceb
thom <tom@tksfz.org>
2017-04-06 12:23:37 -0700

    Print event planned tasks with thread prefixes

--- src/main/scala/org/tksfz/doto/main/ListCmdExec.scala
+++ src/main/scala/org/tksfz/doto/main/ListCmdExec.scala
@@ -34,6 +34,16 @@ abstract class Printer(repo: Project, val sb: StringBuilder = new StringBuilder)
   def allowAnsi = System.console() != null
 }
 
+case class TaskPath(path: Seq[Node[_]], task: Task) {
+  def pathString(allowAnsi: Boolean) = {
+    val color = if (allowAnsi) Console.BLUE + Console.BOLD else ""
+    color + "(" + path.map { _ match {
+        case thread: Thread[_] => s"${thread.icon} ${thread.subject}"
+      }
+    }.mkString(" > ") + ") "
+  }
+}
+
 // List all threads and tasks hierarchically
 // Within a thread:
 //   - print tasks grouped by event
@@ -51,13 +61,13 @@ class DefaultPrinter(project: Project, thread: Thread[_ <: Work]) extends Printe
     thread.asTaskThread foreach { t =>
       if (allowAnsi) sb.append(Console.RESET)
       sb.append("Planned:\n")
-      val tasks = findAllTasks(t)
-      val tasksByEvent = tasks.groupBy(_.target.map(_.id))
+      val tasks = findAllTaskPaths(t, Nil)
+      val tasksByEvent = tasks.groupBy(_.task.target.map(_.id))
       val plannedTasksByEvent = tasksByEvent.collect({ case (Some(eventRef), tasks) => eventRef -> tasks })
       val sortedEvents = project.events.findByIds(plannedTasksByEvent.keys.toSeq).sorted(project.eventsOrdering)
       for(event <- sortedEvents) {
-        if (!isEventPlanTotallyDone(event, plannedTasksByEvent(event.id))) {
-          printEventWithTasks(sb, 1, event, plannedTasksByEvent(event.id))
+        if (!isEventPlanTotallyDone(event, plannedTasksByEvent(event.id).map(_.task))) {
+          printEventWithTaskPaths(1, event, plannedTasksByEvent(event.id))
         }
       }
       sb.append("\n")
@@ -76,12 +86,14 @@ class DefaultPrinter(project: Project, thread: Thread[_ <: Work]) extends Printe
 
   private[this] def indent(depth: Int) = " " * (depth * 2)
 
-  private[this] def printLineItem(depth: Int, item: Node[_], icon: String, color: String): Unit = {
+  private[this] def printLineItem(depth: Int, item: Node[_], icon: String, color: String, prefix: String = ""): Unit = {
     val shortId = item.id.toString.substring(0, 6)
     if (allowAnsi) sb.append(Console.RESET)
     sb.append(shortId + indent(depth) + " ")
     if (allowAnsi) sb.append(color)
-    sb.append(icon + " " + item.subject + "\n")
+    sb.append(icon + " " + prefix)
+    if (allowAnsi) sb.append(color)
+    sb.append(item.subject + "\n")
   }
 
   private[this] def printThread(depth: Int, thread: Thread[_ <: Work], omitThreadLineItem: Boolean = false): Unit = {
@@ -94,12 +106,12 @@ class DefaultPrinter(project: Project, thread: Thread[_ <: Work]) extends Printe
         val tasks = project.tasks.findByIds(thread.children.toIds)
         val unplannedTasks = tasks.filter(!_.isPlanned)
         for(task <- unplannedTasks) {
-          printTask(sb, depth + 1, task)
+          printTask(depth + 1, task)
         }
       case EventWorkType =>
         for(event <- project.events.findByIds(thread.children.toIds)) {
           if (!isNodeTotallyDone(event)) {
-            printEvent(sb, depth + 1, event)
+            printEvent(depth + 1, event)
           }
         }
     }
@@ -125,11 +137,13 @@ class DefaultPrinter(project: Project, thread: Thread[_ <: Work]) extends Printe
   }
 
   // what about event threads?
-  private[this] def findAllTasks(t: Thread[Task]): Seq[Task] = {
-    val myTasks = findAllTasks(t.children)
-    val subthreads = project.findSubThreads(t.id)
-    val subthreadsTasks = subthreads.flatMap(st => st.asTaskThread.toList.flatMap(findAllTasks(_)))
-    myTasks ++ subthreadsTasks
+  private[this] def findAllTaskPaths(t: Thread[Task], prefix: Seq[Node[_]]): Seq[TaskPath] = {
+    val myTaskPaths = findAllTasks(t.children).map(t => TaskPath(prefix, t))
+    val subthreadsTaskPaths =
+      project.findSubThreads(t.id)
+        .collect { case TaskThread(tt) => tt }
+        .flatMap(tt => findAllTaskPaths(tt, prefix :+ tt))
+    myTaskPaths ++ subthreadsTaskPaths
   }
 
   private[this] def findAllTasks(xs: List[Ref[Task]]): Seq[Task] = {
@@ -138,29 +152,38 @@ class DefaultPrinter(project: Project, thread: Thread[_ <: Work]) extends Printe
     tasks ++ recurse
   }
 
-  private[this] def printTask(sb: StringBuilder, depth: Int, task: Task): Unit = {
+  private[this] def printTask(depth: Int, task: Task, prefix: String = ""): Unit = {
     val icon = if (task.completed) "[x]" else "[ ]"
     val color = if (task.completed) Console.GREEN else (Console.GREEN + Console.BOLD)
-    printLineItem(depth, task, icon, color)
-    for(
-      subtask <- project.tasks.findByIds(task.children.toIds)
-      if !subtask.isPlanned
-    ) {
-      printTask(sb, depth + 1, subtask)
+    printLineItem(depth, task, icon, color, prefix)
+    val subtasks = project.tasks.findByRefs(task.children)
+    subtasks
+      .filter(!_.isPlanned) // BUGBUG: printTask() is invoked for planned section too
+      .foreach(printTask(depth + 1, _))
+  }
+
+  private[this] def printTaskPath(depth: Int, taskPath: TaskPath): Unit = {
+    val prefix = taskPath.pathString(allowAnsi)
+    printTask(depth, taskPath.task, prefix)
+  }
+
+  private[this] def printEvent(depth: Int, event: Event): Unit = {
+    printEventLineItem(depth, event)
+    for(subtask <- project.tasks.findByRefs(event.children)) {
+      printTask(depth + 1, subtask)
     }
   }
 
-  private[this] def printEvent(sb: StringBuilder, depth: Int, event: Event): Unit = {
-    printEventWithTasks(sb, depth, event, project.tasks.findByIds(event.children.toIds))
-  }
-
-  private[this] def printEventWithTasks(sb: StringBuilder, depth: Int, event: Event, tasks: Seq[Task]): Unit = {
+  private[this] def printEventLineItem(depth: Int, event: Event) = {
     val icon = if (event.completed) "![x]" else "![ ]"
     val color = if (event.completed) Console.YELLOW else (Console.YELLOW + Console.BOLD)
     printLineItem(depth, event, icon, color)
-    for(subtask <- tasks) {
-      printTask(sb, depth + 1, subtask)
-    }
   }
 
+  private[this] def printEventWithTaskPaths(depth: Int, event: Event, taskPaths: Seq[TaskPath]): Unit = {
+    printEventLineItem(depth, event)
+    for(subtask <- taskPaths) {
+      printTaskPath(depth + 1, subtask)
+    }
+  }
 }
