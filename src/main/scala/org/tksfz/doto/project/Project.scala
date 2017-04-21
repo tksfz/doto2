8bc02e4ba7b12ffcc14b6e5cc1afde613ab9cd88
thom <tom@tksfz.org>
2017-04-21 15:28:36 -0700

    Add delete command

--- src/main/scala/org/tksfz/doto/project/Project.scala
+++ src/main/scala/org/tksfz/doto/project/Project.scala
@@ -63,7 +63,14 @@ class Project(rootPath: Path) {
     this.threads.findAllTaskThreads.find(f) orElse this.tasks.findOne(f)
   }
 
+  def find(f: HasChildren[_ <: HasId] => Boolean) = {
+    val thread: Option[Node[_ <: HasId]] = this.allThreads.find(f)
+    thread orElse this.tasks.findOne(f) orElse this.events.findOne(f)
+  }
+
   def findTaskParent(taskId: Id) = findOneTaskOrTaskThread(_.children.exists(_.id == taskId))
+
+  def findParent(id: Id) = find(_.children.exists(_.id == id))
 
   trait CollByType[T] {
     def coll: Coll[T]
@@ -175,6 +182,11 @@ class Coll[T : Encoder : Decoder](root: ScalaFile) {
 
   def put[A <: HasId](doc: A)(implicit ev: A =:= T): Unit = {
     put(doc.id, doc)
+  }
+
+  def remove(id: Id): Unit = {
+    val file = root / id.toString
+    file.delete()
   }
 
   // TODO: move Ref to here
