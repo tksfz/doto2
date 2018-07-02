4b687255fa6323a3fe8678712998c975d1839805
thom <tom@tksfz.org>
2018-07-02 08:41:28 -0700

    Launch vi to edit some content

---
+++ src/main/scala/org/tksfz/doto/main/EditCmdExec.scala
@@ -0,0 +1,49 @@
+package org.tksfz.doto.main
+
+import better.files._
+
+object EditCmdExec extends CmdExec[EditCmd] {
+  override def execute(c: Config, cmd: EditCmd) = WithActiveProject { project =>
+    project.tasks.findByIdPrefix(cmd.id).map { taskOrEvent =>
+      val header =
+        s"""
+           |You are editing the description field of task taskId: subject
+           |You can use markdown here.
+       """.stripMargin
+
+      val edited = editContent(header + taskOrEvent.description.getOrElse(""))
+
+      //project.put(taskOrEvent.copy(description = Some(contents)))
+      println(edited)
+    }.getOrElse {
+      println(s"Couldn't find task with id ${cmd.id}")
+    }
+  }
+
+  private def editContent(content: String): String = {
+    val f = File.newTemporaryFile("doto-", ".md")
+    f.overwrite(content)
+    runEditor(f.pathAsString)
+    val newContents = f.contentAsString
+    f.delete()
+    newContents
+  }
+
+  private def runEditor(filename: String) = {
+    val editor = sys.env.getOrElse("EDITOR", "vi")
+    runConsoleProcess(editor, filename)
+  }
+
+  // https://stackoverflow.com/questions/9690702/how-can-i-launch-vi-from-within-java-under-commons-exec
+  private def runConsoleProcess(command: String, args: String*) = {
+    import scala.collection.JavaConverters.seqAsJavaListConverter
+    val processBuilder = new ProcessBuilder((command +: args).asJava)
+    processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT)
+    processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT)
+    processBuilder.redirectInput(ProcessBuilder.Redirect.INHERIT)
+
+    val p = processBuilder.start()
+    // wait for termination.
+    p.waitFor()
+  }
+}
