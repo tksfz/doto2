5fa1a5b69a8752b54d8d806de6376b0f6b53fd36
thom <tom@tksfz.org>
2018-07-04 21:33:22 -0700

    Add ModelExtensionsImplicits

---
+++ src/main/scala/org/tksfz/doto/main/ModelExtensionsImplicits.scala
@@ -0,0 +1,18 @@
+package org.tksfz.doto.main
+
+import org.tksfz.doto.model.HasContent
+
+object ModelExtensionsImplicits {
+  val Subject = "subject"
+  val Completed = "completed"
+  val Description = "description"
+
+  implicit class HasContentExtensionMethods[T <: HasContent](val obj: T) extends AnyVal {
+    def subject = obj.field[String](Subject).getOrElse("")
+    def completed = obj.field[Boolean](Completed).getOrElse(false)
+
+    def withSubject(s: String) = obj.withContentField(Subject, s)
+
+    def withCompleted(f: Boolean) = obj.withContentField(Completed, f)
+  }
+}
