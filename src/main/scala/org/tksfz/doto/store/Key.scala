e5f34bfb04a9b1f1f557e9e573c3c2fd3eed5058
thom <tom@tksfz.org>
2018-07-07 15:15:53 -0700

    Move Key[Id] type class instance to model package

--- src/main/scala/org/tksfz/doto/store/Key.scala
+++ src/main/scala/org/tksfz/doto/store/Key.scala
@@ -1,9 +1,6 @@
 package org.tksfz.doto.store
 
-import java.nio.file.{Path, Paths}
-import java.util.UUID
-
-import org.tksfz.doto.model.Id
+import java.nio.file.Path
 
 abstract class HasKey[T, K](implicit val key: Key[K]) {
   def key(t: T): K
@@ -13,11 +10,3 @@ trait Key[K] {
   def toPath(k: K): Path
   def fromPath(p: Path): K
 }
-
-object Key {
-  implicit val idKey = new Key[Id] {
-    override def toPath(k: Id): Path = Paths.get(k.toString)
-    override def fromPath(p: Path): Id = UUID.fromString(p.toString)
-  }
-}
-
