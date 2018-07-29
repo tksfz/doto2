0e5c7d92cf9c7242b2b929ac1d0ed348d31d99b7
thom <tom@tksfz.org>
2018-07-29 16:01:46 -0700

    List all the CmdExec objects as implicits

---
+++ src/main/scala/org/tksfz/doto/main/package.scala
@@ -0,0 +1,21 @@
+package org.tksfz.doto
+
+package object main {
+  implicit val addCmdExec: CmdExec[Add] = AddCmdExec
+  implicit val cloneCmdExec: CmdExec[Clone] = CloneCmdExec
+  implicit val completeCmdExec: CmdExec[Complete] = CompleteCmdExec
+  implicit val deleteCmdExec: CmdExec[Delete] = DeleteCmdExec
+  implicit val editCmdExec: CmdExec[EditCmd] = EditCmdExec
+  implicit val focusCmdExec: CmdExec[Focus] = FocusCmdExec
+  implicit val helpCmdExec: CmdExec[HelpCmd] = HelpCmdExec
+  implicit val initCmdExec: CmdExec[Init] = InitCmdExec
+  implicit val listCmdExec: CmdExec[ListCmd] = ListCmdExec
+  implicit val newCmdExec: CmdExec[New] = NewCmdExec
+  implicit val projectCmdExec: CmdExec[ProjectCmd] = ProjectCmdExec
+  implicit val scheduleCmdExec: CmdExec[Schedule] = ScheduleCmdExec
+  implicit val setCmdExec: CmdExec[SetCmd] = SetCmdExec
+  implicit val statusCmdExec: CmdExec[StatusCmd] = StatusCmdExec
+  implicit val syncCmdExec: CmdExec[Sync] = SyncCmdExec
+  implicit val threadCmdExec: CmdExec[ThreadCmd] = ThreadCmdExec
+  implicit val viewCmdExec: CmdExec[ViewCmd] = ViewCmdExec
+}
